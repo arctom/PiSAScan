bfd0ad32c0ed1995958661c2c6c7ada562426c67
arctom <arctomb@users.noreply.github.com>
2024-05-28 10:12:50 -0600

    webpage

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import PiSAScan from './PiSAScan';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PiSAScan />
+    </div>
+  );
+}
+
+export default App;
+
