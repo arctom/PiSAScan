a6c67f851b0586e5bdfd94a9ebc1efa9495009f4
arctom <arctomb@users.noreply.github.com>
2024-06-06 11:54:10 -0600

    codigo.py update

--- codigo.py
+++ codigo.py
@@ -16,7 +16,9 @@ from sentence_transformers import SentenceTransformer
 from spire.doc import *
 from spire.doc.common import *
 from unidecode import unidecode
-from tempfile import NamedTemporaryFile 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import balanced_accuracy_score
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
@@ -37,7 +39,9 @@ def main_code(file_path):
         output_path = 'D:/Github/PiSAScan/data/output.xlsx'
         
         embeddings, processed_data = process_file(file_path, poppler_path=poppler_path)
-        save_to_excel(embeddings, [processed_data], output_path)
+        df_embeddings, df_cvs = save_to_df(embeddings, [processed_data], output_path)
+        
+        test_model(df_embeddings, df_cvs, [0], None, None, None, None, None, None, None, None, None)
         
         from main import show_results
         show_results(output_path)
@@ -140,17 +144,14 @@ def process_pdf(file_path, poppler_path):
         print(f"Error processing PDF file {file_path}: {e}")
         return ""
 
-def save_to_excel(embeddings, cvs, filename):
-    """Save embeddings and CVs to an Excel file.""" 
+def save_to_df(embeddings, cvs, filename):
+    """Save embeddings and CVs to 2 df's.""" 
     try:
         df_embeddings = pd.DataFrame(embeddings)
         df_cvs = pd.DataFrame(cvs)
-        with pd.ExcelWriter(filename, engine='openpyxl') as writer:
-            df_embeddings.to_excel(writer, sheet_name='Embeddings', index=False)
-            df_cvs.to_excel(writer, sheet_name='CVs', index=False)
-        print(f"Data successfully saved to {filename}")
+        return df_embeddings, df_cvs
     except Exception as e:
-        print(f"Error saving to Excel file {filename}: {e}")
+        print(f"Error saving to file {filename}: {e}")
 
 def split_text(text, chunk_size=4999):
     """
@@ -199,3 +200,111 @@ def process_file(file_path, label='cv', num_words=7, poppler_path='/usr/local/op
 
     embeddings, processed_text = preprocess_by_fixed_words(text, label, 0, num_words)
     return embeddings, {'label': label, 'index': 0, 'text': processed_text}
+
+def get_clusters_features(df, clusters_coeff):
+    """
+    This function gets the features for the clusters
+    """
+    for column in df.columns:
+        if str(column) in clusters_coeff.columns:
+            df[column] *= clusters_coeff[str(column)].iloc[0]
+
+    return df
+
+def get_individual_words_features(df, words_coeff):
+    """
+    This function extract features
+    """
+    # We get the features for c_idf
+    df_copy = df.copy()
+    df_copy['combined_sentences'] = df.groupby('index')['sentence'].transform(lambda x: " ".join(x))
+    corpus = df_copy.drop_duplicates(subset=['index', 'combined_sentences'])
+    print('Corpus Shape')
+    print(corpus.shape)
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(corpus['combined_sentences'])
+
+    columns = vectorizer.get_feature_names_out()
+    cols = words_coeff.columns
+    print('Cols')
+    print(cols)
+    X = X.toarray()
+    result = np.zeros((X.shape[0], words_coeff.shape[1]))
+    for i, col in enumerate(cols):
+        if col in columns:
+            index = -1
+            for it in range(len(columns)):
+                if columns[it] == col:
+                    index = it
+                    break
+            if index != -1:
+                result[:, i] = (X[:, index] * words_coeff[col].iloc[0])
+
+    return result
+
+def get_test_matrix(df, df_norm, num_clusters, clusters_coeff, words_coeff):
+    """
+    """
+    keys = df['index'].unique()
+    X = {k: np.zeros(num_clusters+1) for k in keys}
+    
+    for _, row in df.iterrows():
+        X[row['index']][row['cluster']] += 1
+
+    for key, value in X.items():
+        X[key][:-1] = X[key][:-1] / df_norm.iloc[key] if df_norm.iloc[key] != 0 else X[key][:-1]
+        X[key][-1] = df_norm.iloc[key] / 100
+    
+    df_result = pd.DataFrame(X).T
+
+    clusters_features = np.array(get_clusters_features(df_result, clusters_coeff))
+    words_features = np.array(get_individual_words_features(df, words_coeff))
+
+    print('Cluster features')
+    print(clusters_features.shape)
+    print('Words features')
+    print(words_features.shape)
+    # Merge clusters_features and words_features together
+    df_result = np.concatenate([clusters_features, words_features], axis=1)
+    print('DfResult')
+    print(df_result.shape)
+
+    return df_result
+
+def test_model(df_embeddings, df_cvs, test_index, lda_model, clf_model, clf_clusters, pca, mask, n_clusters, clusters_coeff, words_coeff):
+    """
+    This function evaluates a model
+    """
+    X_test = df_embeddings[df_embeddings['index'].isin(test_index)].copy()
+    y_test = df_cvs[df_cvs['index'].isin(test_index)].copy()['label']
+    df_cvs_copy = df_cvs[df_cvs['index'].isin(test_index)].copy()
+
+    print('Step 2:: Generating Synthetic Data')
+    rows_gerente = df_cvs_copy[df_cvs_copy['label'] == 'Gerente'].shape[0]
+    print(f'Rows Gerente:: {rows_gerente}')
+
+    cluster_prediction = clf_clusters.predict(X_test['embedding'].to_list())
+    X_test['cluster'] = cluster_prediction
+
+    X = get_test_matrix(X_test, X_test['len'], n_clusters, clusters_coeff, words_coeff)
+
+    print('Step::Apply PCA')
+    X = pca.transform(X)
+
+    # print('Step::Apply mask')
+    # X_pro = X.loc[:, mask]
+
+    print('Step::Apply LDA')
+    # X_points = lda_model.transform(np.array(X_pro))
+    X_points = lda_model.transform(np.array(X))
+
+    print('Step::10 Predict Proba')
+    print(y_test)
+    print(clf_model.predict_proba(X_points))
+    score = balanced_accuracy_score(y_test, clf_model.predict(X_points))
+    cm = confusion_matrix(y_test, clf_model.predict(X_points), labels=clf_model.classes_)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm,
+                              display_labels=clf_model.classes_)
+    disp.plot()
+    # plt.show()
+    return None
